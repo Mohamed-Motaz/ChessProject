a05165e651814aedaa52f7084c7a5fcfd42fefbd
Mohamed Motaz <mohamedmotaz247@yahoo.com>
2020-03-22 19:14:23 +0200

    Update

---
+++ ChessProject/Gameplay.cpp
@@ -0,0 +1,18 @@
+#include "Gameplay.h"
+#include "getAvailableSquares.h"
+#include "chessPiece.h"
+#include "Board.h"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <SFML/Graphics.hpp>>
+
+bool Gameplay::IsValidPositionChange(pair<ll, ll> startingPosition, pair<ll, ll> targetPosition)
+{
+	return false;
+}
+
+pair<pair<ll, ll>, pair<ll, ll>> Gameplay::InputStartingPositionAndTargetPosition()
+{
+	return pair<pair<ll, ll>, pair<ll, ll>>();
+}
---
+++ ChessProject/Gameplay.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sfml/Graphics.hpp>
+#include "getAvailableSquares.h"
+#include "chessPiece.h"
+#include "Board.h"
+
+#define ll long long
+
+using namespace std;
+using namespace sf;
+
+class Gameplay
+{
+private:
+	Gameplay();
+public:
+	
+	static bool IsValidPositionChange(pair<ll, ll> startingPosition, pair<ll, ll> targetPosition);
+
+	static pair<pair<ll, ll>, pair<ll, ll>> InputStartingPositionAndTargetPosition();
+	
+
+};
